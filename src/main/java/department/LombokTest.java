c7d5f416b0fce5991e3b88717f9c280547584f74
Xlopec <maksimolejn720@gmail.com>
2017-01-25 22:18:46 +0200

    updated pom.xml

---
+++ src/main/java/department/LombokTest.java
@@ -0,0 +1,8 @@
+package department;
+
+import lombok.Value;
+
+@Value
+public class LombokTest {
+	int field;
+}
--- src/main/java/department/Test.java
+++ src/main/java/department/Test.java
@@ -4,8 +4,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -17,20 +15,15 @@ public class Test extends Application {
         launch(args);
     }
 
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
 		primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        btn.setOnAction(event -> {
+				System.out.println("Hello");
+		});
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
@@ -43,6 +36,11 @@ public class Test extends Application {
 		ISomeService service = (ISomeService) context.getBean("SomeService");
 		
 		service.f();
+		
+		
+		// enable annotation processing first in compiler options!
+		LombokTest lombok = new LombokTest(1);
+		System.out.println(lombok.getField());
 	}
 
 }
