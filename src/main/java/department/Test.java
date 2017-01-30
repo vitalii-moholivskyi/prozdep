5b7833a7ea6495becb917bc4342d52234c821dd4
Roman Nevmerzhitskiy <rvlnev@gmail.com>
2017-01-31 01:02:37 +0200

    view template

--- src/main/java/department/Test.java
+++ src/main/java/department/Test.java
@@ -1,13 +1,15 @@
 package department;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test extends Application {
 	
@@ -24,11 +26,18 @@ public class Test extends Application {
         btn.setOnAction(event -> {
 				System.out.println("Hello");
 		});
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+
+		BorderPane root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+		Tab _tabList = FXMLLoader.load(getClass().getResource("/view/partials/_listTab.fxml"));
+		Tab _tabCreateMaster = FXMLLoader.load(getClass().getResource("/view/partials/_createMasterTab.fxml"));
+		TabPane contentData = (TabPane) root.lookup("#contentPane");
+		_tabList.setText("Список");
+		_tabCreateMaster.setText("Створити");
+		contentData.getTabs().add(_tabList);
+		contentData.getTabs().add(_tabCreateMaster);
+		Scene scene = new Scene(root, 800, 600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"appContext.xml");
